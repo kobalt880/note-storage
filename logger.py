3b3bc931088bb95cf142b10255677f663b4209f8
Arseny <kobalt8800@gmail.com>
2025-03-06 22:16:51 +0300

    пофиксил баг в функции configure_user

--- logger.py
+++ logger.py
@@ -121,16 +121,16 @@ def delete_password(user, change=False):
         print('у вас не установлен пароль')
 
 def find_password(user):
-        bool = False
-        
-        with open('passwords.txt', 'r', encoding='utf-8') as f:
-            text = f.readlines()
+    bool = False
+    
+    with open('passwords.txt', 'r', encoding='utf-8') as f:
+        text = f.readlines()
 
-            for e in text:
-                if e[:len(user)+1] == f'{user}:':
-                    bool = True
+        for e in text:
+            if e[:len(user)+1] == f'{user}:':
+                bool = True
 
-        return bool
+    return bool
 
 def find_user(user):
     bool = True
@@ -198,11 +198,11 @@ def configure_user(user):
             with open(f"notes\\{new_name}_notes.txt", "w", encoding="utf-8") as f:
                 f.write(text)
         except: pass
-
-        return new_name
     
         if find_password(old_name):
             create_password(new_name, change=True, old_user=old_name)
+
+        return new_name
 
 def delete_user(user, glob=False):
     name = user
